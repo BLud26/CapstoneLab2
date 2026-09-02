c37ad2ae72bd3ff44ac0b58345d8e05dda3b7b5d
BLud26 <jx8385eq@go.minnstate.edu>
2026-09-02 17:51:54 -0500

    Part 2 complete

--- author_no_duplicates.py
+++ author_no_duplicates.py
@@ -3,8 +3,12 @@ class Author:
         self.name = name
         self.books = []
 
+    # Adds book title to author's list, unless it's already there
     def publish(self, title):
-        self.books.append(title)
+        if title in self.books: #checks if title is already in list
+            print(f"Error: You already entered {title} for {self.name}")
+        else:
+            self.books.append(title) # Only runs if the title is new
 
     def __str__(self):
         return f"{self.name} has published: {', '.join(self.books)}"
@@ -13,6 +17,7 @@ def main():
     tolkien = Author("J.R.R Tolkien")
     tolkien.publish("The Hobbit")
     tolkien.publish("The Fellowship of the Ring")
+    tolkien.publish("The Hobbit") #Duplicate
 
     rowling = Author("J.K. Rowling")
     rowling.publish("The Prisoner of Azkaban")
