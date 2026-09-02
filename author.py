fd45af807cdc2a84c6ceba8e707a77ea290fd809
BLud26 <jx8385eq@go.minnstate.edu>
2026-09-02 17:44:10 -0500

    Forgot to add comments, so they're in now

--- author.py
+++ author.py
@@ -1,14 +1,21 @@
+# Author object
 class Author:
+
+    # This will run automatically when you create a new author
+    # Self is the specific author being used at that moment
     def __init__(self, name):
         self.name = name
-        self.books = []
+        self.books = [] # Each author starts with empty book list
 
+    # Adds book title to author's list
     def publish(self, title):
-        self.books.append(title)
+        self.books.append(title) # Adds to the end of the list
 
+    # Runs automatically when an author is printed
     def __str__(self):
-        return f"{self.name} has published: {', '.join(self.books)}"
+        return f"{self.name} has published: {', '.join(self.books)}" #.join will put the list of titles into one string separated by commas
 
+# Call and test above methods
 def main():
     tolkien = Author("J.R.R Tolkien")
     tolkien.publish("The Hobbit")
@@ -21,4 +28,4 @@ def main():
     print(rowling)
 
 if __name__ == "__main__":
-        main()
+    main()
